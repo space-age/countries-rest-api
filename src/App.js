2bce1faad1cc87300e858702a40add075e1298e4
space-age <53498728+space-age@users.noreply.github.com>
2022-08-25 12:28:39 -0700

    final build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Title from "./components/General/Title";
+import SearchInput from "./components/SearchInput/SearchInput";
+import LoadSpinner from "./components/General/LoadingSpinner";
+import Results from "./components/Results/Results";
+import Navigation from "./components/Navigation/Navigation";
+import Pagination from "./components/Pagination/Pagination";
+import ErrorMessage from "./components/General/ErrorMessage";
+
+import StateContext from "./state/state-context";
+import { useContext, useMemo, useState } from "react";
 
 function App() {
+  const ctx = useContext(StateContext);
+
+  //////////////////////////////////////////////////////////////////////////
+  //Pagination
+  const [paginationResults, setPaginationResults] = useState([]);
+
+  const getPaginationResults = useMemo(() => {
+    setPaginationResults(ctx.getSearchResultsPage(ctx.paginationPage));
+    window.scroll(0, 0); //pushes window to the top
+  }, [ctx.search, ctx.paginationPage]);
+  //////////////////////////////////////////////////////////////////////////
+
+  const resultsEmpty =
+    ctx.search.results === undefined || ctx.search.results.length === 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title={"React REST Countries API Project"} />
+      <Navigation />
+      <SearchInput />
+      {ctx.loading && <LoadSpinner />}
+      {ctx.searchErrorFound && (
+        <ErrorMessage message="not found, please try again!" />
+      )}
+      {resultsEmpty && <Results result={paginationResults} />}
+      <Pagination />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { StateContextProvider } from "./state/state-context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateContextProvider>
+      <App />
+    </StateContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
